e2dcca12c49cf87a773825b8feb6133ca6b0d636
Mateusz Wos <Mateusz.Wos@gds.ey.com>
2024-03-15 18:07:14 +0100

    Add methods, annotations in GlobalExceptionHandler class

--- src/main/java/cma/api/exceptions/GlobalExceptionHandler.java
+++ src/main/java/cma/api/exceptions/GlobalExceptionHandler.java
@@ -4,13 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-
 
     @ExceptionHandler({ContactNotFoundException.class})
     public ResponseEntity<Object> handleContactNotFoundException(ContactNotFoundException exception) {
@@ -19,6 +15,10 @@ public class GlobalExceptionHandler {
                 .body(exception.getMessage());
     }
 
-
-
+    @ExceptionHandler({ValidationException.class})
+    public ResponseEntity<Object> handleValidationException(ValidationException exception) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(exception.getMessage());
+    }
 }
