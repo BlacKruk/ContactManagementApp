f37f614eeb0e3c5459cba416f212cfe32e494937
Mateusz Wos <Mateusz.Wos@gds.ey.com>
2024-03-27 16:06:39 +0100

    Make changes to CMAController

--- src/main/java/cma/api/controller/CMAController.java
+++ src/main/java/cma/api/controller/CMAController.java
@@ -8,7 +8,6 @@ import cma.api.service.CMAService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import org.springframework.validation.annotation.Validated;
