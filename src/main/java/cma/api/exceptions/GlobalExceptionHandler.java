481c155d0e59c07ced78183fddaf131b92c45392
Mateusz Wos <Mateusz.Wos@gds.ey.com>
2024-03-28 16:31:25 +0100

    Make changes to GlobalExceptionHandler

--- src/main/java/cma/api/exceptions/GlobalExceptionHandler.java
+++ src/main/java/cma/api/exceptions/GlobalExceptionHandler.java
@@ -8,8 +8,9 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -20,16 +21,14 @@ public class GlobalExceptionHandler {
                 .status(HttpStatus.NOT_FOUND)
                 .body(exception.getMessage());
     }
+
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public @ResponseBody Map<String, String> handleValidationExceptions(
             MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        return errors;
+
+        return ex.getBindingResult().getAllErrors().stream()
+                .map(error -> (FieldError) error)
+                .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
     }
 }
